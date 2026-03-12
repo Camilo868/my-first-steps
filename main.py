3252a0bda25a4c58d8a3c05d872d6ac1f6d7b9b5
Camilo868 <camiloramirezosorio12345@gmail.com>
2026-03-12 09:38:48 -0500

    Implement student registration and grade input

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-camilo =16
-print(camilo)
+Students=[]
+subjects = ["Math", "Biology", "Socials"]
+print("\n1 Welcome to my program about register the sutednts")
+diferent_student=int(input("\n1 How many students do you want to registeer: "))
+for student in range (diferent_student):
+    grades={}
+    name=input(f"What is the name of the student? {student+1}. ")
+    
+    for idx in range(len(subjects)):
+        grade = float(input(f"Enter the grade for {subjects[idx]}: "))
+        grades[subjects[idx]] = grade 
+
+    Students.append({
+        "name":name,
+        "grades":grades
+    })
+
+for idx, mostrar in enumerate(Students):
+    print(f"{idx+1}. The student {mostrar["name"]} {mostrar["grades"]}")
